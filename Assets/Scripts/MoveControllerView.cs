8eea109a9ca9ffc3a28f92bc74270d369b715b20
shulaeu <shulaeu@gmail.com>
2023-10-30 21:06:29 +0300

    111

--- Assets/Scripts/MoveControllerView.cs
+++ Assets/Scripts/MoveControllerView.cs
@@ -8,6 +8,7 @@ public class MoveControllerView : MonoBehaviour
 {
     private const float delay = 1.2f;
 
+    [SerializeField] private Rigidbody rigidbody;
     [SerializeField] private float speed = 7f;
     [SerializeField] private float force = 8f;
 
@@ -15,26 +16,28 @@ public class MoveControllerView : MonoBehaviour
 
     private float lastJump;
 
-    void Update()
+    private void Update()
     {
         float x = Input.GetAxis("Horizontal");
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
 
-        if (Mathf.Abs(x) > 0.1f)
+        if (Mathf.Abs(x) > 0.01f)
         {
-            Vector3 velocity = GetComponent<Rigidbody>().angularVelocity;
+            Vector3 velocity = rigidbody.velocity;
             velocity = new Vector3(x * speed, velocity.y, velocity.z);
-            GetComponent<Rigidbody>().velocity = velocity;
+            rigidbody.velocity = velocity;
         }
 
         if (!isInJump && Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody>().AddForce(0, force, 0, ForceMode.Impulse);
+            rigidbody.AddForce(0, force, 0, ForceMode.Impulse);
             isInJump = true;
+            anim.SetBool("Jump",true);
         }
+    }
 
-        if (Mathf.Abs(GetComponent<Rigidbody>().angularVelocity.y) <= 0f)
-        {
-            isInJump = false;
-        }
+    private void OnCollisionEnter(Collision other)
+    {
+        isInJump = false;
     }
 }
