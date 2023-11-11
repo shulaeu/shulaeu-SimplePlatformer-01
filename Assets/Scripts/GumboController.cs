fbef59e9196331c5138bc76857c4f32d1f02a40d
shulaeu <shulaeu@gmail.com>
2023-11-12 01:19:56 +0300

    Gumbo was added

---
+++ Assets/Scripts/GumboController.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+using Unity.System.Random;
+
+public class GumboController : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.1f;
+    [SerializeField] private float movement = 0.1f;
+    [SerializeField] private float i = transform.position.z;
+    //i = GetComponent<transform.position.z>;
+    
+    public Transform[] movePoints;
+    private int randomPoint;
+    private float waitTime;
+    public float startWaitTime;
+
+    //[Range (-4.5f,5.5f) SerializeField] private float distance;
+
+    private void Start()
+    {
+        randomPoint = Random.Range(0, movePoints.Length);
+        waitTime = startWaitTime;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoints[randomPoint].position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoints[randomPoint].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomPoint = Random.Range(0, movePoints.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Timeout.deltaTime;
+            }
+        }
+
+        //if (transform.position.z < 5.0f && transform.position.z >= 0f)
+        //{
+        //    transform.position += new Vector3(0,0,movement) * speed * Time.deltaTime;
+        //}
+        //else if (transform.position.z > -5.5f)
+        //{
+        //    transform.position += new Vector3(0,0,-movement) * speed * Time.deltaTime;
+        //}
+
+        //for (transform.position.z = 0f; transform.position.z <5.0f; transform.position += new Vector3(0, 0, movement) * speed * Time.deltaTime)
+        //{
+        //    //transform.position += new Vector3(0, 0, movement) * speed * Time.deltaTime;
+        //}
+        //for (transform.position.z = 5.0f; transform.position.z > -5.5f; transform.position += new Vector3(0, 0, -movement) * speed * Time.deltaTime)
+        //{
+        //    //transform.position += new Vector3(0, 0, movement) * speed * Time.deltaTime;
+        //}
+        
+        //for (i = 0; i < 5.0f; i++)
+        //{
+        //    transform.position += new Vector3(0, 0, movement) * speed * Time.deltaTime;
+        //}
+        //for (i = 0; i < 5.0f; i--)
+        //{
+        //    transform.position += new Vector3(0, 0, -movement) * speed * Time.deltaTime;
+        //}
+        
+        
+
+
+    }
+}
--- Assets/Scripts/MoveControllerView.cs
+++ Assets/Scripts/MoveControllerView.cs
@@ -2,42 +2,79 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.ComponentModel.Design.Serialization;
+//using System.Diagnostics.Eventing.Reader;
 using UnityEngine;
 
 public class MoveControllerView : MonoBehaviour
 {
-    private const float delay = 1.2f;
+    //private const float delay = 1.2f;
+    private const float rotateAngle = 0.7f;
 
-    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private GameObject root;
+
+    //[SerializeField] private Rigidbody rigidbody;
     [SerializeField] private float speed = 7f;
     [SerializeField] private float force = 8f;
+    [SerializeField] private float rotateForce = 200f;
 
     [SerializeField] private bool isInJump;
 
     private float lastJump;
+    private Animator anim;
+    private Rigidbody rigidbody;
+    private static readonly int Jump = Animator.StringToHash("Jump");
+    private static readonly int Speed = Animator.StringToHash("Speed");
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
         float x = Input.GetAxis("Horizontal");
-        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        //Rigidbody rigidbody = GetComponent<Rigidbody>();
 
         if (Mathf.Abs(x) > 0.01f)
         {
             Vector3 velocity = rigidbody.velocity;
             velocity = new Vector3(x * speed, velocity.y, velocity.z);
             rigidbody.velocity = velocity;
+            anim.SetFloat(Speed, Mathf.Abs(x));
+
+            if (x > 0)
+            {
+                if(root.transform.rotation.y<rotateAngle)
+                {
+                    root.transform.RotateAround(root.transform.position, Vector3.up, rotateForce * Time.deltaTime);
+                }
+            }
+            else if(x<0)
+            {
+                if (root.transform.rotation.y < rotateAngle)
+                {
+                    root.transform.RotateAround(root.transform.position, Vector3.up, -rotateForce * Time.deltaTime);
+                }
+            }
         }
 
         if (!isInJump && Input.GetButtonDown("Jump"))
         {
             rigidbody.AddForce(0, force, 0, ForceMode.Impulse);
             isInJump = true;
-            anim.SetBool("Jump",true);
+            anim.SetBool("Jump", true);
+        }
+        else
+        {
+            anim.SetFloat(Speed,0);
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
         isInJump = false;
+        anim.SetBool("Jump", false);
     }
 }
